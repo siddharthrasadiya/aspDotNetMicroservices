3c0fab2a3dd16ad515aade6388079a5b5e49ac4e
siddharth <siddharthpatel3474@gmail.com>
2023-05-10 16:21:16 +0530

    basket microservices devloped

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task<IEnumerable<Product>> GetProductsByCategory(string categoryName);
+        Task CreateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+        Task<bool> UpdateProduct(Product product);
+
+
+
+    }
+}
